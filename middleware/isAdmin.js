1f2eac79a5ba3a302e28541789493ae9f259ed28
AishuN1107 <naidusaiaishu2003@gmail.com>
2025-05-25 11:44:22 +1000

    Setup Roles and Design Admin login form

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,8 +1,9 @@
 function isAdmin(req, res, next) {
+  console.log('Checking session:', req.session.user);
   if (req.session?.user?.role === 'admin') {
-    return next(); // Allow access
+    return next();
   } else {
-    return res.redirect('/admin/login'); // Block access
+    return res.redirect('/admin/login');
   }
 }
 module.exports = isAdmin;
