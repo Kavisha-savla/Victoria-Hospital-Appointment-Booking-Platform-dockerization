86143506d9d15e994e11c61490ebca359fa29ed8
kavishasavla12 <kavisha.savla16034@sakec.ac.in>
2025-05-26 16:10:24 +1000

    admin dashboard layout and export csv feature

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const path = require('path'); // THIS WAS MISSING
+const path = require('path');
 const router = express.Router();
 const isAdmin = require('../middleware/isAdmin');
 router.get('/dashboard', isAdmin, (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/adminLogin.html'));
+  console.log('Admin dashboard accessed by:', req.session.user);
+  res.sendFile(path.join(__dirname, '../views/adminDashboard.html'));
 });
 module.exports = router;
